50d36021b12db7802eb9072b0a39b1d93c2bfd17
PaulaSmiles <157551773+PaulaSmiles@users.noreply.github.com>
2024-05-13 21:37:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,14 @@ st.write("""
 # My first app
 Hello *world!*
 """)
- 
-import streamlit as st
-import pandas as pd
+
+st.set_page_config(
+    page_title="Percent change for Pueblo, CO",
+    page_icon="✅",
+    layout="wide",
+)
+
+
 
 # Load the CSV data
 csv_file = "percentage_change.csv"
@@ -16,3 +21,5 @@ percentage_change_data = pd.read_csv(csv_file)
 # Display the chart
 st.line_chart(percentage_change_data)
 
+# top-level filters
+job_filter = st.selectbox("Select the Category", pd.unique(df["Category"]))
